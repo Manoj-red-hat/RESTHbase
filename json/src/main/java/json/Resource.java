fa8fb136898e767cf68bfe4960d79e41e71a6077
Manoj Kumar <manoj.kumar.mbm@gmail.com>
2016-11-21 22:38:35 +0530

    Updated GET and POST

--- json/src/main/java/json/Resource.java
+++ json/src/main/java/json/Resource.java
@@ -1,8 +1,12 @@
 package json;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("home")
@@ -12,5 +16,18 @@ public class Resource {
   @Produces(MediaType.TEXT_PLAIN)
   public String helloWorld() {
     return "Hello, world!";
+  }
+  @GET
+  @Path("param")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String paramMethod(@QueryParam("name") String name) {
+    return "Hello, " + name;
+  }
+  @POST
+  @Path("post")
+  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+  @Produces(MediaType.TEXT_HTML)
+  public String postMethod(@FormParam("name") String name) {
+    return "<h2>Hello, " + name + "</h2>";
   }
 }
